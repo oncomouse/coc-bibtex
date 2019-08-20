8467aaef447928c1f3be6872c463c2e3c1583a30
oncomouse <oncomouse@gmail.com>
2019-08-20 15:23:24 -0500

    I Can't figure out why this doesn't work

--- src/bibtex.ts
+++ src/bibtex.ts
@@ -1,57 +1,33 @@
-import { ChildProcess, spawn } from 'child_process'
-import { BasicList, ListTask, Neovim, workspace } from 'coc.nvim'
+import { BasicList, ListTask, ListItem, Neovim, workspace } from 'coc.nvim'
 import { EventEmitter } from 'events'
-import readline from 'readline'
-import { executable } from './util'
-import { installFzfBibTeX, goBinPath } from './commands'
+import fs from 'fs'
+import Cite from './vendor/citation-js'
 
 class Task extends EventEmitter implements ListTask {
-  private processes: ChildProcess[] = []
+  private processes: Promise<ListItem>[] = []
 
-  public start(cmd: string, files: string[], outputCmd: string): void {
-    let remain = files.length
+  public start(files: string[]): void {
     for (let file of files) {
-      const process = spawn(cmd, [file])
-      this.processes.push(process)
-      const outputProcess = spawn(outputCmd, [file])
-      this.processes.push(outputProcess)
-      process.on('error', e => {
-        this.emit('error', e.message)
-      })
-      const rl = readline.createInterface(process.stdout)
-      process.stderr.on('data', chunk => {
-        console.error(chunk.toString('utf8')) // tslint:disable-line
-      })
-
-      rl.on('line', (line: string) => {
-        const id = `@${line.replace(/\x1b\[[0-9;]*m/g,'').split('@').slice(-1)[0]}`.trim()
-        outputProcess.stdout.once('data', (cite) => {
-          this.emit('data', {
-            label: line,
-            filterText: id,
+      const bibData = Cite.parse.bibtex.text(fs.readFileSync(file).toString())
+      bibData.forEach(entry => {
+        this.processes.push(new Promise<ListItem>(resolve => {
+          const cite = (new Cite(entry, {})).format('bibliography', {append: entry => `[${entry.id}]`})
+          const data = {
+            label: cite,
+            filterText: entry.id,
             data: {
-              cite: cite.toString().trim(),
+              cite: `@${entry.id}`
             }
-          })
-        })
-        outputProcess.stdin.write(id)
-      })
-      rl.on('close', () => {
-        remain = remain - 1
-        outputProcess.stdin.end()
-        if (remain == 0) {
-          this.emit('end')
-        }
+          }
+          this.emit('data', data)
+          resolve(data)
+        }))
       })
     }
+    Promise.all(this.processes).then(() => this.emit('end'))
   }
 
   public dispose(): void {
-    for (let process of this.processes) {
-      if (!process.killed) {
-        process.kill()
-      }
-    }
   }
 }
 
@@ -67,7 +43,6 @@ export default class FilesList extends BasicList {
     super(nvim)
     this.addAction('insert', async (item) => {
       const {nvim} = workspace
-      workspace.showMessage(`normal! i [${item.data.cite}]`)
       await nvim.command(`normal! i [${item.data.cite}]`)
       await nvim.call('feedkeys', ['a', 'n'])
     })
@@ -87,26 +62,11 @@ export default class FilesList extends BasicList {
       this.files.push(fullPath)
     }
   }
-  public async getCommand(): Promise<{ cmd: string, outCmd: string }> {
-    const config = workspace.getConfiguration('list.source.bibtex')
-    let cmd = await goBinPath('bibtex-ls')
-    if (!executable(cmd)) {
-      const installed = await installFzfBibTeX()
-      if(!installed) {
-        workspace.showMessage('There was a problem installing fzf-bibtex.', 'error')
-        return null
-      }
-    }
-    const outCmd = await goBinPath(config.get<string>('outputCommand', 'bibtex-cite'))
-    return { cmd, outCmd }
-  }
 
   public async loadItems(): Promise<ListTask> {
-    const res = await this.getCommand()
-    if (!res) return null
     if (this.files.length === 0) return null
     const task = new Task()
-    task.start(res.cmd, this.files, res.outCmd)
+    task.start(this.files)
     return task
   }
 }
