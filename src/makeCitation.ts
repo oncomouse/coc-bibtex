c6b0e494a2eb186b9f8c83d9d141907dcb68c830
oncomouse <oncomouse@gmail.com>
2019-08-22 13:23:34 -0500

    external function

---
+++ src/makeCitation.ts
@@ -0,0 +1,14 @@
+import { workspace } from 'coc.nvim'
+
+interface CitationConfiguration {
+  before: string,
+  after: string,
+}
+
+const makeCitation = (id:string):string => {
+  const config = workspace.getConfiguration('list.source.bibtex')
+  const {before,after} = config.get<CitationConfiguration>('citation', {before: '[@', after: ']'})
+  return `${before}${id}${after}`
+}
+
+export default makeCitation
