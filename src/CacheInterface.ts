1a76d74601cad48836ac0ecb34fd45af27b27f9c
oncomouse <oncomouse@gmail.com>
2019-11-17 13:41:34 -0600

    Caching now looks at mtime to determine if file has changed.

--- src/CacheInterface.ts
+++ src/CacheInterface.ts
@@ -6,7 +6,8 @@ import {createHash} from 'crypto'
 class CacheInterface {
   private static cachePath = path.join(homedir(),'.config', 'coc', 'extensions', 'bibtex', '.cache')
   private static fileHash(filename:string, algorithm = 'md5'):string {
-    return createHash(algorithm).update(filename).digest("hex")
+    // Build in last known change:
+    return createHash(algorithm).update(filename + fs.statSync(filename).mtime).digest("hex")
   }
   private static createCacheDirectory() {
     if(!fs.existsSync(CacheInterface.cachePath)) {
